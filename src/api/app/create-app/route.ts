5fd0e3efb1504fb6cbb0007a6541221a27fcc935
Vineet Pandey <vineetpandey0090@gmail.com>
2026-04-30 21:07:27 +0530

    added api endpoints

---
+++ src/api/app/create-app/route.ts
@@ -0,0 +1,20 @@
+import { prisma } from "@/lib/db/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const { name, config_json, userId } = await req.json();
+        const app = await prisma.app.create({
+            data: {
+                name,
+                config_json,
+                userId,
+            },
+        })
+        return NextResponse.json(app, { status: 200 });
+    }
+    catch (err) {
+        console.log(err);
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    }
+}
