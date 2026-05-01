1d1c27501956b62f5b7f79e1ccb30e979b7e9318
Vineet Pandey <vineetpandey0090@gmail.com>
2026-05-01 21:53:47 +0530

    update: added db call to get app data and store in AppStore

--- src/components/auth/AuthGuard.tsx
+++ src/components/auth/AuthGuard.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useAuth, SignIn } from "@clerk/nextjs";
+import axios from "axios";
+import { useAppStore } from "../../store/appStore";
 
 export default function AuthGuard({
   children,
@@ -8,8 +10,22 @@ export default function AuthGuard({
   children: React.ReactNode;
 }) {
   const { isSignedIn, isLoaded } = useAuth();
+  const { appData, setAppData } = useAppStore();
 
   if (!isLoaded) return null;
+
+  if(isSignedIn && appData.length === 0) {
+    try {
+      const fetchAppsData = async () => {
+        const res = await axios.get("/api/app/get-apps");
+        console.log(res.data);
+        setAppData(res.data);
+      };
+      fetchAppsData();
+    } catch (error) {
+      console.error("Error fetching apps data from db:", error);
+    }
+  }
 
   if (!isSignedIn) {
     return (
